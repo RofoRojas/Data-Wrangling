f8ca86a411e5f830c1a48d0b722ee9703f4f78c7
RofoRojas <rofito66@gmail.com>
2019-09-09 01:37:01 -0600

    Laboratorio 5
    El formato y un poco las graficas me jugaron un mal rato

---
+++ Clase05/Clase 5.R
@@ -0,0 +1,20 @@
+install.packages("nycflights13")
+library(lubridate)
+library(nycflights13)
+
+
+today()
+
+
+now()
+
+date_string <- "1994 October 27th 09:00:43"
+
+as.Date(date_string)
+
+ymd_hms(date_string)
+
+as.Date(0001)
+
+make_datejm()
+make_datetime(2019, 12, 31)
